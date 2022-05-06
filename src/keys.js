b732b6230941bea15402249ca4a39f65b56a5084
pablo <portega017@ikasle.ehu.es>
2022-05-06 11:19:23 +0200

    cambio en base de datos para funcionar con docker

--- src/keys.js
+++ src/keys.js
@@ -1,6 +1,6 @@
 module.exports={ //exportamos un objeto
     database:{ //este objeto contiene la config de la BD
-        host: '192.168.1.14',
+        host: 'localhost',
         user:'adminacad',
         password:'prueba@1A',
         database:'academia'
