28a9e8bd11f423b7f7a0f5bd0624190c484b8bc8
pablo <portega017@ikasle.ehu.es>
2022-05-10 01:06:06 +0200

    cambio dirección BD

--- src/keys.js
+++ src/keys.js
@@ -1,6 +1,6 @@
 module.exports={ //exportamos un objeto
     database:{ //este objeto contiene la config de la BD
-        host: 'pibol.duckdns.org',
+        host: '172.18.0.1',
         user:'adminacad',
         password:'prueba@1A',
         database:'academia'
